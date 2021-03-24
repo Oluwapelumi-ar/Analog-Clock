206c48125d9bd7c13d07be2ade0d73003079713d
Shukurah <bakareshukurah@outlook.com>
2021-03-24 09:47:51 +0100

    edit

--- analog.js
+++ analog.js
@@ -4,27 +4,22 @@ const secondHand = document.querySelector("#second");
 
 
 // Get Hour,minutes and seconds using date methods
-
-
-  //Moving the clock hands using javascript
-  // hrPosition = hrPosition+(30/3600);
-  // minPosition = minPosition + (6/60);
-  // secPosition = secPosition+6;
-
-
-
-function runTheClock() { 
-  
-  const date = new Date ();
+const date = new Date ();
   console.log(date);
   let hr = date.getHours();
   let min = date.getMinutes();
   let sec = date.getSeconds();
 
-
+  //Moving the clock hands using javascript
   let hrPosition = (hr*360/12)+(min*(360/60)/12);
   let minPosition = (min*360/60)+(sec*(360/60)/60);
   let secPosition = sec*360/60;
+
+
+function runTheClock() { 
+  hrPosition = hrPosition+(30/3600);
+  minPosition = minPosition + (6/60);
+  secPosition = secPosition+6;
 
   hourHand.style.transform =   'rotate(' + hrPosition  + 'deg)';
   minuteHand.style.transform = 'rotate(' + minPosition + 'deg)';
